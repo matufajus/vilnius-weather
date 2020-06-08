6ca2990037b72955bfda03c916f01f27f0f6e7b4
matufajus <matas.tas@gmail.com>
2020-06-08 19:17:43 +0300

    Initializing spring boot project

---
+++ src/main/java/com/javatask/vilniusweather/VilniusWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.javatask.vilniusweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilniusWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VilniusWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatask/vilniusweather/VilniusWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatask.vilniusweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilniusWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
